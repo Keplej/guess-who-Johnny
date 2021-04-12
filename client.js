82c5d3e0c9b7b9eafe2c162a2a050b3c88cbf85d
Johnny Keple <Johnny.Keple@gmail.com>
2021-04-12 16:03:28 -0500

    Initial Commit

--- client.js
+++ client.js
@@ -1,1 +1,18 @@
 console.log('Here are all the available people:', people);
+
+$(document).ready(onReady);
+
+let persons = 0;
+
+function onReady() {
+    console.log('Testing');
+    
+}
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+function () {
+    
+}
